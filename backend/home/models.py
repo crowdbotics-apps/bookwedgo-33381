a0a40970c888f46639f64ab8aa9ed3f2e569eccb
Crowdbotics <team@crowdbotics.com>
2022-02-11 06:51:48 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,18 @@
+from django.conf import settings
 from django.db import models
+
+
+class TodoList(models.Model):
+    "Generated Model"
+    taskname = models.CharField(
+        max_length=256,
+    )
+    taskdescription = models.TextField()
+    duedate = models.DateTimeField()
+    username = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="todolist_username",
+    )
